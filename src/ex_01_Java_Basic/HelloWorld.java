e6d07f6138af3d50aa41d8e56ea8175ddf2bf87b
Shubham <gunjalshubham2416@gmail.com>
2025-01-30 00:27:09 +0530

    30 jan

---
+++ src/ex_01_Java_Basic/HelloWorld.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basic;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+
+        int a = 1;
+        int b = 2;
+        int c;
+        c = a + b;
+        System.out.println("C is " + c);
+    }
+}
